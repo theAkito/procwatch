4c2224e966f1b9c82752d6cc376aceb2da4286e8
Akito <the@akito.ooo>
2022-02-11 00:10:44 +0100

    Add Global Constants

---
+++ src/meta.nim
@@ -0,0 +1,8 @@
+const
+  debug             * {.booldefine.} = false
+  appVersion        * {.strdefine.}  = "0.1.0"
+  configName        * {.strdefine.}  = "procwatch.json"
+  configPath        * {.strdefine.}  = ""
+  configIndentation * {.intdefine.}  = 2
+  dirProc           *                = "/proc"
+  
