c23a898a61a852d70044a5b5e2c71ea4787b6081
Akito <the@akito.ooo>
2022-02-11 00:11:44 +0100

    Add Config Manager

---
+++ src/configurator.nim
@@ -0,0 +1,87 @@
+import
+  meta,
+  json,
+  base64,
+  os,
+  logging,
+  strutils
+
+type
+  ProcwatchConfig = object
+    version                   *: string
+    useMail                   *: bool
+    useDesktop                *: bool
+    useMattermost             *: bool
+    useMatrix                 *: bool
+    useRocketChat             *: bool
+    useRevoltChat             *: bool
+    useMumble                 *: bool
+    mailNameSender            *: string
+    mailUsername              *: string
+    mailPassword              *: string
+    mailSubject               *: string
+    mailMessage               *: string
+    mailPortOutgoing          *: int
+    mailSmtpServerOutgoing    *: string
+    mailAddressSource         *: string
+    mailAddressTarget         *: string
+    dbusNameApp               *: string
+    dbusSummary               *: string
+    dbusMessage               *: string
+    dbusNameIcon              *: string
+    dbusTimeout               *: int32
+    mattermostURL             *: string
+    mattermostLoginID         *: string
+    mattermostPassword        *: string
+    mattermostToken           *: string
+    mattermostChannelID       *: string
+    mattermostMessage         *: string
+    mattermostRootID          *: string
+    mattermostFileIDs         *: seq[string]
+    mattermostProperties      *: JsonNode
+    debug                     *: bool
+
+let logger = newConsoleLogger(lvlInfo, "[$levelname]:[$datetime] ~ ")
+
+var config* = ProcwatchConfig(
+  version: appVersion,
+  mailPortOutgoing: 587,
+  dbusNameApp: "Process Watcher",
+  dbusSummary: "Process Finished",
+  dbusMessage: "Watched process finished executing.",
+  dbusTimeout: 15_000,
+  dbusNameIcon: "help-faq",
+  mattermostURL: "http://localhost:8065/api/v4",
+  mattermostProperties: parseJson("{}"),
+  debug: meta.debug
+)
+
+func pretty(node: JsonNode): string = node.pretty(configIndentation)
+
+func genPathFull(path, name: string): string =
+  if path != "": path.normalizePathEnd() & '/' & name else: name
+
+proc getConfig*(): ProcwatchConfig = config
+
+proc genDefaultConfig(path = configPath, name = configName): JsonNode =
+  let
+    pathFull = path.genPathFull(name)
+    conf = %* config
+  pathFull.writeFile(conf.pretty())
+  conf
+
+proc initConf*(path = configPath, name = configName): bool =
+  let
+    pathFull = path.genPathFull(name)
+    configAlreadyExists = pathFull.fileExists
+  if configAlreadyExists:
+    logger.log(lvlDebug, "Config already exists! Not generating new one.")
+    config = pathFull.parseFile().to(ProcwatchConfig)
+    config.mailPassword = config.mailPassword.decode()
+    config.mattermostPassword = config.mattermostPassword.decode()
+    return true
+  try:
+    genDefaultConfig(path, name)
+  except:
+    return false
+  true
