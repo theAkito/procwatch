44574ff54c2cb5d378f18bdf22ba6a99e52d75e8
Akito <the@akito.ooo>
2022-02-13 15:51:41 +0100

    Improve Mattermost Error Handling & Health Check & Refactor

--- src/externnotifapi/mattermost.nim
+++ src/externnotifapi/mattermost.nim
@@ -1,12 +1,17 @@
+## https://api.mattermost.com/#operation/CreatePost
+
 import
   ../meta,
   json,
   strutils,
+  apiutils,
   os,
   logging,
   puppy
 
 type
+  MattermostDefect = object of Defect
+
   MattermostContext* = ref object
     url          * : string      # Example: http://localhost:8065/api/v4
     loginID      * : string      # Only necessary, if using a temporary session token, instead of a voluntarily permanent Bearer token.
@@ -18,10 +23,16 @@ type
     fileIDs      * : seq[string] # Optional
     properties   * : JsonNode    # Optional
 
+const apiPathRoot = "/api/v4"
+
 let logger = newConsoleLogger(defineLogLevel(), logMsgPrefix & logMsgInter & nameMattermost & logMsgSuffix)
 
-proc postMattermost*(ctx: MattermostContext): bool =
-  ctx.url.normalizePathEnd()
+func appendApiPathRoot(baseUrl: string): string =
+  if not baseUrl.endsWith("apiPathRoot"): baseUrl.normalizePathEnd() & apiPathRoot else: baseUrl
+func prepApiUrl(ctx: MattermostContext) = ctx.url = appendApiPathRoot(ctx.url)
+
+proc apiMsgPost(ctx: MattermostContext): JsonNode =
+  ctx.prepApiUrl()
   let
     req = Request(
       url: parseUrl(ctx.url & "/posts"),
@@ -30,6 +41,16 @@ proc postMattermost*(ctx: MattermostContext): bool =
       body: "{ \"channel_id\": \"$#\", \"message\": \"$#\", \"root_id\": \"$#\", \"file_ids\": \"$#\", \"props\": \"$#\" }" % [ctx.channelID, ctx.message, ctx.rootID, $(% ctx.fileIDs), $ctx.properties]
     )
     resp = req.fetch()
-    respCode = $resp.code
-  logger.log(lvlDebug, resp.body.parseJson().pretty)
-  respCode.startsWith("20")
+    respCode = resp.code
+    jResp =
+      try: resp.body.parseJson()
+      except: raise MattermostDefect.newException("Unable to post a message due to JSON parsing error!")
+    respBody = jResp.pretty
+  if respCode.is20x():
+    logger.log(lvlDebug, respBody)
+  else:
+    logger.log(lvlError, respBody)
+    raise MattermostDefect.newException("Unable to post a message due to an API error!")
+  jResp
+
+proc postMattermost*(ctx: MattermostContext): bool = apiMsgPost(ctx){"status_code"} == nil
